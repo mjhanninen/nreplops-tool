9462e78ff5e9bebff74eec374cd830099d786702
Matti Hänninen <matti@mjhanninen.com>
2022-05-01 20:59:32 +0300

    Add missing notices

--- src/bin/test_hosts_file.rs
+++ src/bin/test_hosts_file.rs
@@ -1,3 +1,18 @@
+// bin/test_hosts_file.rs
+// Copyright 2022 Matti Hänninen
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
 use nreplops_tool::hosts_files::*;
 
 fn main() {
