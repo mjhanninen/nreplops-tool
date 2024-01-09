373570694189f8e549c6ab705e10239bcc1cc31b
Matti Hänninen <matti@mjhanninen.com>
2024-01-09 05:45:35 +0200

    Add forgotten test file

---
+++ nr-clj-lexing-tester/input/simple14.clj
@@ -0,0 +1,14 @@
+;; simple14.clj -- tagged literals
+
+#inst"2022-01-01"
+#inst "2022-01-01"
+#
+;; intermittent comments …
+inst
+;; … are allowed.
+"2022-01-01"
+
+#foo [:bar :baz]
+#foo {:bar "baz"}
+
+# ^:foo tag ^:bar #_ {} "arg"
